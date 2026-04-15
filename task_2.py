eae93b327f55db70db8aa883d0cd2f52e434a7e2
maximivasiv <maximivasiv1@gmail.com>
2026-04-15 20:38:16 +0300

    Create task_2.py

---
+++ task_2.py
@@ -0,0 +1,15 @@
+import random
+
+def get_numbers_ticket(min, max, quantity):
+    if (
+        min < 1 or
+        max > 1000 or
+        min > max or
+        quantity < 1 or
+        quantity > (max - min + 1)
+    ):
+        return []
+
+    numbers = random.sample(range(min, max + 1), quantity)
+    numbers.sort()
+    return numbers
